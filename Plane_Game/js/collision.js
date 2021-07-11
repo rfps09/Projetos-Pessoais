35fd72435ac3a2629b735e271e69ab3a015fd20c
Rafael <rafael09felipe@gmail.com>
2021-07-11 15:21:42 -0300

    Update collision.js

--- Plane_Game/js/collision.js
+++ Plane_Game/js/collision.js
@@ -16,7 +16,7 @@ function colisao(objeto1, objeto2) {
         {x: objeto2.x + objeto2.width, y: objeto2.y + objeto2.height}
     ]
 
-    while ( (colidiu==false) && (indice<3) ) {
+    while ( (colidiu==false) && (indice<4) ) {
         ((pontos_obj1[indice].x >= objeto2.x && pontos_obj1[indice].x <= objeto2.x + objeto2.width && pontos_obj1[indice].y >= objeto2.y && pontos_obj1[indice].y <= objeto2.y + objeto2.height) ||
         (pontos_obj2[indice].x >= objeto1.x && pontos_obj2[indice].x <= objeto1.x + objeto1.width && pontos_obj2[indice].y >= objeto1.y && pontos_obj2[indice].y <= objeto1.y + objeto1.height)) ? colidiu = true: indice++
     }
